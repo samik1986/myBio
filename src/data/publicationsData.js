beb8f401dc933029d2b20077685c2b65e72e3cd2
samik1986 <samik.banerjee.howrah@gmail.com>
2026-06-11 16:00:31 +0530

    Update publications metadata from tex resume

--- src/data/publicationsData.js
+++ src/data/publicationsData.js
@@ -1,33 +1,11 @@
 import img_0 from '../assets/drive_3D_multimodal_histological_atlas_and_coordinate_fr.jpeg';
-import img_1 from '../assets/drive_A_guide_to_the_BRAIN_Initiative_Cell_Census_Networ.jpeg';
-import img_2 from '../assets/drive_A_multimodal_cell_census_and_atlas_of_the_mammalia.jpeg';
-import img_3 from '../assets/drive_A_threedimensional_histological_cell_atlas_of_the_.png';
-import img_4 from '../assets/drive_Cellular_anatomy_of_the_mouse_primary_motor_cortex.png';
-import img_5 from '../assets/drive_Deep_Domain_Adaptation_for_Face_Recognition_using_.jpeg';
-import img_6 from '../assets/drive_Detecting_aircrafts_from_satellite_images_using_sa.jpeg';
-import img_7 from '../assets/drive_Detection_and_skeletonization_of_single_neurons_an.jpeg';
-import img_8 from '../assets/drive_Domain_Adaptation_with_Softmargin_multiple_feature.png';
-import img_9 from '../assets/drive_DPGAN_Dual_Pathway_Generative_Adversarial_Network_.png';
-import img_10 from '../assets/drive_Eigen_Domain_Transformation_for_Softmargin_Multipl.png';
-import img_11 from '../assets/drive_Face_recognition_in_surveillance_conditions_with_b.png';
-import img_12 from '../assets/drive_gtcx1.png';
-import img_13 from '../assets/drive_High_precision_automated_detection_of_labeled_nucl.jpeg';
-import img_14 from '../assets/drive_Kernel_selection_using_multiple_kernel_learning_an.png';
-import img_15 from '../assets/drive_LRGAN_for_Degraded_Face_Recognition.png';
-import img_16 from '../assets/drive_MakeUpMirror_Mirroring_makeups_and_verifying_faces.jpeg';
-import img_17 from '../assets/drive_Mutual_variation_of_information_on_TransferCNN_for.jpeg';
-import img_18 from '../assets/drive_Performance_of_Deep_Learning_Algorithms_vs_Shallow.jpeg';
-import img_19 from '../assets/drive_PosIXGAN_Generating_multiple_poses_using_GAN_for_p.png';
-import img_20 from '../assets/drive_Reversible_Data_Encryption_Algorithm.jpeg';
-import img_21 from '../assets/drive_Reversible_masking_a_novel_faultdiagnosed.jpeg';
-import img_22 from '../assets/drive_Secure_scan_design_with_Isomorphic_registers.jpeg';
-import img_23 from '../assets/drive_Semantic_segmentation_of_microscopic_neuroanatomic.png';
-import img_24 from '../assets/drive_Skeletonization_of_neuronal_processes_using_Discre.png';
-import img_25 from '../assets/drive_SoftMargin_Learning_for_Multiple_FeatureKernel_Com.jpeg';
-import img_26 from '../assets/drive_Solving_the_where_problem_and_quantifying_geometri.png';
-import img_27 from '../assets/drive_Solving_the_where_problem_and_quantifying_geometri.png';
-import img_28 from '../assets/drive_SpoofNET_Resolving_Facial_Makeup_based_Spoofs.png';
-import img_29 from '../assets/drive_Unsupervised_method_of_domain_adaptation_on_repres.png';
+import img_1 from '../assets/drive_Eigen_Domain_Transformation_for_Softmargin_Multipl.png';
+import img_2 from '../assets/drive_MakeUpMirror_Mirroring_makeups_and_verifying_faces.jpeg';
+import img_3 from '../assets/drive_Mutual_variation_of_information_on_TransferCNN_for.jpeg';
+import img_4 from '../assets/drive_PosIXGAN_Generating_multiple_poses_using_GAN_for_p.png';
+import img_5 from '../assets/drive_Reversible_Data_Encryption_Algorithm.jpeg';
+import img_6 from '../assets/drive_Reversible_masking_a_novel_faultdiagnosed.jpeg';
+import img_7 from '../assets/drive_Secure_scan_design_with_Isomorphic_registers.jpeg';
 
 export const publications = [
   {
@@ -89,8 +67,8 @@ export const publications = [
     id: "pub-6",
     title: "Deep Domain Adaptation for Face Recognition using images captured from surveillance cameras",
     authors: "Samik Banerjee et al.",
-    journal: "IEEE / Computer Vision Conference",
-    year: 2019,
+    journal: "BioSIG, ACM, Darmstadt, Germany, December",
+    year: 2018,
     isPreprint: false,
     link: "#",
     image: img_5,
@@ -100,8 +78,8 @@ export const publications = [
     id: "pub-7",
     title: "Detecting aircrafts from satellite images using saliency and conical pyramid based template representation",
     authors: "Samik Banerjee et al.",
-    journal: "Journal / Conference",
-    year: 2020,
+    journal: "Sādhanā (Academy Proceedings in Engineering Science), Springer, Vol. 41(10), Pages 1155–71, October",
+    year: 2016,
     isPreprint: false,
     link: "#",
     image: img_6,
@@ -112,7 +90,7 @@ export const publications = [
     title: "Detection and skeletonization of single neurons and tracer injections using topological methods",
     authors: "Samik Banerjee et al.",
     journal: "Medical Image Analysis / Bioinformatics",
-    year: 2022,
+    year: 2004,
     isPreprint: false,
     link: "#",
     image: img_7,
@@ -122,8 +100,8 @@ export const publications = [
     id: "pub-9",
     title: "Domain Adaptation with Soft-margin multiple feature-kernel learning beats Deep Learning for surveillance face recognition",
     authors: "Samik Banerjee et al.",
-    journal: "IEEE / Computer Vision Conference",
-    year: 2019,
+    journal: "arXiv preprint, arXiv:1610.01374, October",
+    year: 2016,
     isPreprint: false,
     link: "#",
     image: img_8,
@@ -133,8 +111,8 @@ export const publications = [
     id: "pub-10",
     title: "DP-GAN- Dual Pathway Generative Adversarial Network for Face Recognition in Degraded Scenarios",
     authors: "Samik Banerjee et al.",
-    journal: "IEEE / Computer Vision Conference",
-    year: 2019,
+    journal: "Indian Conference on Computer Vision, Graphics and Image Processing (ICVGIP), ACM, IIIT Hyderabad, India, December",
+    year: 2018,
     isPreprint: false,
     link: "#",
     image: img_9,
@@ -144,19 +122,19 @@ export const publications = [
     id: "pub-11",
     title: "Eigen Domain Transformation for Soft-margin Multiple Feature-Kernel Learning for Surveillance Face Recognition",
     authors: "Samik Banerjee et al.",
-    journal: "IEEE / Computer Vision Conference",
-    year: 2019,
+    journal: "Workshop on Computer Vision Applications (WCVA),  9th Indian Conference on Computer Vision, Graphics and Image Processing (ICVGIP), Springer, pp. 180-191, IIT Guwahati, Assam, India",
+    year: 2016,
     isPreprint: false,
     link: "#",
-    image: img_10,
+    image: img_1,
     abstract: "Read the full paper for more details."
   },
   {
     id: "pub-12",
     title: "Face recognition in surveillance conditions with bag-of-words, using unsupervised domain adaptation",
     authors: "Samik Banerjee et al.",
-    journal: "IEEE / Computer Vision Conference",
-    year: 2019,
+    journal: "9th Indian Conference on Computer Vision, Graphics and Image Processing (ICVGIP), IISc. Bangalore, India, ACM, pp. 50",
+    year: 2014,
     isPreprint: false,
     link: "#",
     image: img_11,
@@ -177,8 +155,8 @@ export const publications = [
     id: "pub-14",
     title: "High precision automated detection of labeled nuclei in Gigapixel resolution image data of Mouse Brain",
     authors: "Samik Banerjee et al.",
-    journal: "Journal / Conference",
-    year: 2020,
+    journal: "bioRxiv 252247, Cold Spring Harbor Laboratory, USA, January",
+    year: 2019,
     isPreprint: false,
     link: "#",
     image: img_13,
@@ -188,8 +166,8 @@ export const publications = [
     id: "pub-15",
     title: "Kernel selection using multiple kernel learning and domain adaptation in reproducing kernel hilbert space, for face recognition under surveillance scenario",
     authors: "Samik Banerjee et al.",
-    journal: "IEEE / Computer Vision Conference",
-    year: 2019,
+    journal: "arXiv preprint, arXiv:1610.00660, October",
+    year: 2016,
     isPreprint: false,
     link: "#",
     image: img_14,
@@ -214,7 +192,7 @@ export const publications = [
     year: 2018,
     isPreprint: false,
     link: "#",
-    image: img_16,
+    image: img_2,
     abstract: "Read the full paper for more details."
   },
   {
@@ -225,15 +203,15 @@ export const publications = [
     year: 2018,
     isPreprint: false,
     link: "#",
-    image: img_17,
+    image: img_3,
     abstract: "Read the full paper for more details."
   },
   {
     id: "pub-19",
     title: "Performance of Deep Learning Algorithms vs. Shallow Models, in Extreme Conditions - Some Empirical Studies",
     authors: "Samik Banerjee et al.",
-    journal: "Journal / Conference",
-    year: 2020,
+    journal: "7th  Perception and Machine Intelligence (PReMI), ACM, ISI Kolkata, India",
+    year: 2017,
     isPreprint: false,
     link: "#",
     image: img_18,
@@ -247,40 +225,40 @@ export const publications = [
     year: 2018,
     isPreprint: false,
     link: "#",
-    image: img_19,
+    image: img_4,
     abstract: "Read the full paper for more details."
   },
   {
     id: "pub-21",
     title: "Reversible Data Encryption Algorithm",
     authors: "Samik Banerjee et al.",
-    journal: "Journal / Conference",
-    year: 2020,
+    journal: "Advances in Computer Science (ACEEE), pp. 40-43, AET-ACS",
+    year: 2012,
     isPreprint: false,
     link: "#",
-    image: img_20,
+    image: img_5,
     abstract: "Read the full paper for more details."
   },
   {
     id: "pub-22",
     title: "Reversible masking- a novel fault-diagnosed",
     authors: "Samik Banerjee et al.",
-    journal: "Journal / Conference",
-    year: 2020,
+    journal: "Advances in Recent Technologies in Communication and Computing (ARTCOM), IEEE, pp. 67-71",
+    year: 2011,
     isPreprint: false,
     link: "#",
-    image: img_21,
+    image: img_6,
     abstract: "Read the full paper for more details."
   },
   {
     id: "pub-23",
     title: "Secure scan design with Isomorphic registers",
     authors: "Samik Banerjee et al.",
-    journal: "Journal / Conference",
-    year: 2020,
+    journal: "Advances in Recent Technologies in Communication and Computing (ARTCOM), IEEE, pp. 67-71",
+    year: 2011,
     isPreprint: false,
     link: "#",
-    image: img_22,
+    image: img_7,
     abstract: "Read the full paper for more details."
   },
   {
@@ -321,7 +299,7 @@ export const publications = [
     title: "Solving the where problem and quantifying geometric variation in neuroanatomy using generative diffeomorphic mapping - Supplementary",
     authors: "Samik Banerjee et al.",
     journal: "Nature Communications",
-    year: 2025,
+    year: 2024,
     isPreprint: false,
     link: "#",
     image: img_26,
@@ -332,7 +310,7 @@ export const publications = [
     title: "Solving the where problem and quantifying geometric variation in neuroanatomy using generative diffeomorphic mapping",
     authors: "Samik Banerjee et al.",
     journal: "Nature Communications",
-    year: 2025,
+    year: 2024,
     isPreprint: false,
     link: "#",
     image: img_27,
@@ -342,11 +320,11 @@ export const publications = [
     id: "pub-29",
     title: "SpoofNET- Resolving Facial Makeup based Spoofs",
     authors: "Samik Banerjee et al.",
-    journal: "Journal / Conference",
-    year: 2020,
+    journal: "Indian Conference on Computer Vision, Graphics and Image Processing (ICVGIP), ACM, IIIT Hyderabad, India, December",
+    year: 2018,
     isPreprint: false,
     link: "#",
-    image: img_28,
+    image: img_2,
     abstract: "Read the full paper for more details."
   },
   {
@@ -354,7 +332,7 @@ export const publications = [
     title: "Survey of loss functions for training deep-learning models in computer vision, categorized based on tasks, metric-forms and learning paradigms",
     authors: "Samik Banerjee et al.",
     journal: "SSRN (Under Review in CVIU)",
-    year: 2025,
+    year: 2006,
     isPreprint: true,
     link: "#",
     image: '',
@@ -364,8 +342,8 @@ export const publications = [
     id: "pub-31",
     title: "Unsupervised method of domain adaptation on representation of discriminatory regions of the face image for surveillance face datasets",
     authors: "Samik Banerjee et al.",
-    journal: "IEEE / Computer Vision Conference",
-    year: 2019,
+    journal: "2nd  Perception and Machine Intelligence (PerMIn), IISc. Kolkata, India, ACM, pp. 123-130",
+    year: 2015,
     isPreprint: false,
     link: "#",
     image: img_29,
